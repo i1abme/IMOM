bdb0be59742c00be9acaf6adb41b0b3b3ae8125b
1abme <124783124+1abme@users.noreply.github.com>
2024-03-14 09:31:42 +0900

    로고추가

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Link, Outlet } from "react-router-dom";
 import NavMenu from "./NavMenu";
 import UserMenu from "./UserMenu";
 import AskBtn from "../AskBtn";
+import Logo from "/public/logo.svg";
 
 const Header = () => {
   return (
@@ -10,7 +11,12 @@ const Header = () => {
         <div className="justify-center flex top-0 h-[179px] items-end pb-[10px] w-full">
           <div className="flex gap-[30px] flex-grow-[0.3] items-center">
             <Link to={"/"}>
-              <h1 className="logo text-[32px] text-main-color">아이맘투어</h1>
+              <div
+                className="w-[224px] h-[70px] bg-center bg-cover"
+                style={{
+                  backgroundImage: `url(${Logo})`,
+                }}
+              />
             </Link>
           </div>
           <UserMenu />
