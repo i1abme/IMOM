efd10ff7c779acab46d8dba5e6653247f72d0eb7
1abme <124783124+1abme@users.noreply.github.com>
2024-04-05 14:48:30 +0900

    .

--- src/components/common/Header/MbHeader.tsx
+++ src/components/common/Header/MbHeader.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import Logo from "/public/sublogo.svg";
 import IconAsk from "/public/icon_mb_ask.svg";
 declare global {
   interface Window {
@@ -22,7 +21,7 @@ const MbHeader = () => {
     <div className="w-full h-[65px] bg-white fixed z-50 top-0 hidden max-xsm:flex items-center justify-center px-[20px]">
       <div className="flex justify-center items-center w-full">
         <Link to="/" className="w-[167px] h-[44px] flex">
-          <img src={Logo} alt="logo" />
+          <img src={"/subLogo.svg"} alt="logo" />
         </Link>
       </div>
       <button
